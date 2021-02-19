de51a40ee41a3edb73dd6e1416c020fa142a2508
eunseon <kwon9348@naver.com>
2021-02-19 23:51:04 +0900

    Reusable Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React from "react";
+
+function Food({favorite}){
+  return <h1>I like {favorite}</h1> 
+}
+
 function App() {
   return (
     <div >
       <h1>hello!</h1>
+      <Food favorite="kimchi"/>
+      <Food favorite="ramen"/>
+      <Food favorite="sangiopsal"/>
+      <Food favorite="chukumi"/>
     </div> 
   );
 }
